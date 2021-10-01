b9d3fc4cb56d7735a63d3ea7dc5f1d0a4d8133f4
Vedant VN <81044922+Vedant-VN@users.noreply.github.com>
2021-10-01 17:43:59 +0530

    OwO

--- translation.py
+++ translation.py
@@ -1,13 +1,13 @@
 class Translation(object):
-    START_TEXT = """ <b>OwO Hey There {}, \nI'm Ai Hayasaka VN's Waifu & A Bot, I can Rename+Convert Multiple Files At Once With Permanent Thumbnail And Custom Caption support Yay! 😁</b> \n
+    START_TEXT = """ <b>OwO Hey There {}, \nI'm [Ai Hayasaka](https://telegra.ph/file/3ad072d8e83028e75c4ee.jpg) VN's Waifu & A Bot, Checkout Help Button to Know more Yay! 😁</b> \n
 
-<b>I'm Well Maintained By: @Vedant_vn 👀✌</b> \n 
+<b>My Dear Owner: @Vedant_vn 👀✌</b> \n 
 """
 
-    BANNED_USER_TEXT = "Lol My Owner Has Banned You From Using Me. That Means You Can't Run Me Now! \n \n Contact : @NarutoRobot_Support To Know More..." 
-    DOWNLOAD_START = "<b>Oh ya Baby OvO, Downloading Your File To My server! Just Wait a Bit</b>"
-    UPLOAD_START = "<b>Downloading Is Now Completed,Okay I'm Uploading Your File into Our PM 😉</b>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Heya Thank you for Using Me Support Our Channel @AnimeSigma❤️</b>"
+    BANNED_USER_TEXT = "Lol My Dear Owner Has Banned You From Using Me. That Means You Can't Run Me Now! kek \n \n Contact : @NarutoRobot_Support To Know More..." 
+    DOWNLOAD_START = "<b>Oh ya Baby 💕 OvO, Downloading Your File To My server! Just Wait a Bit...</b>"
+    UPLOAD_START = "<b>Ayee Downloading Is Now Completed,Okay I'm Uploading Your File into Our PM 😉</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Heya Thank you for Using Me Support Our Channel @AnimeSigma 💖</b>"
     SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /delthumb ❤"
     DEL_ETED_CUSTOM_THUMB_NAIL = "Ok Reeee...thumbnail cleared succesfully🤦 Khush Abb?"
     SAVED_RECVD_DOC_FILE = "<b>Yay File Downloaded Successfully 😎</b>"
@@ -16,8 +16,8 @@ class Translation(object):
     CUSTOM_CAPTION_UL_FILE = " "
     NO_THUMB_FOUND = "Aur Bhai Thumbnail Kab Daaloge?"
     IFLONG_FILE_NAME = """Oi Pagal...Decrease The Number Of Letters,Chutiya Salla 🙂"""
-    ABOUT_ME = """<b>UwU \nHey I'm Ai Hayasaka 🙈 Just Another Telegram File Renamer Bot Which Can Rename Telegram Files. \nWell,I Support Permanent Thumbnail Feature.\n \ni can also convert files into videos \nStill Got Any Issues With Me HUH?🤨 Contact @NarutoRobot_Support & Tag @Vedant_Vn there For Your Answers 🙂 </b>"""
-    HELP_USER = """Checkout The Available Commands Here \n\nDo Join @AnimeSigma If You Found Me Useful Darling❤️"""
+    ABOUT_ME = """<b>UwU \nHey I'm [Ai Hayasaka](https://telegra.ph/file/817feecc5e1561cbe1173.mp4) 🙈 Just Another Telegram File Renamer Bot Which Can Rename Telegram Files. \nWell, I Support Permanent Thumbnail Feature.\n \nI can also convert files into videos \nStill Got Any Issues With Me HUH?🤨 Contact @NarutoRobot_Support & Tag @Vedant_Vn there For Your Answers 🙂 </b>"""
+    HELP_USER = """Checkout The Available Commands Here \n\nAnd And...👉🏻👈🏻 please Join @AnimeSigma If You Found Me Useful Darling 💝"""
     RENAME_HELP = """Here are The Available Commands In Rename \n\n\n▪️ <code>/rename</code> : Reply To An File/video With <code>/rename Filename.extension</code> For Renaming"""
     C2V_HELP = """Here Are The Available Commands In File To Video \n\n\n ▪️<code>/c2v</code> : Reply To An File With /c2v To Convert It Into Video"""
     THUMBNAIL_HELP = """Here Are The Available Commands In Custom Thumbnail \n\n\n ▪️ Send A Photo To Set The Custom Thumbnail \n▪️ <code>/showthumb</code> : For Checking The Current Thumbnail \n▪️<code>/delthumb</code> : For Deleting The Current Saved Thumbnail"""
